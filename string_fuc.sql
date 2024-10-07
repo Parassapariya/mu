cfb99a335ac7a59cebcc6e4cf851ad8612199eda
paras <parassapariya212@gmail.com>
2024-10-07 09:50:53 +0530

    sqlfuction

---
+++ string_fuc.sql
@@ -0,0 +1,13 @@
+
+select length('java');
+
+select concat('java','123')
+select trim('  java    ')
+select ltrim('  java')
+select rtrim('java    ')
+select LPAD('paras','10','$')
+select RPAD('paras','10','$')
+SELECT LOCATE('cat', 'concatenation');
+SELECT SUBSTR('Hello World', 7);
+
+
